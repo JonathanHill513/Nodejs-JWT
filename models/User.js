ba53a6fe60edb708530b6c14f256ac3cc464b1cc
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-11-30 08:54:07 -0500

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,17 +17,25 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// fire a function after doc saved to db
-userSchema.post('save', function (doc, next) {
-  console.log('new user was created & saved', doc);
+//fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-// fire a function before doc saved to db
-userSchema.pre('save', function (next) {
-  console.log('user about to be created & saved', this);
-  next();
-});
+//static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
 const User = mongoose.model('user', userSchema);
 
